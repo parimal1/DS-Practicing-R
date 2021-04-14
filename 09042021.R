f89254b8352e1e7c0131c59c2f33f0bdc2b48602
Parimal Gujarathi <parimalgujarathi@hotmail.com>
2021-04-14 16:52:25 +0530

    Add files via upload
    Practice session of R language on dated 09/04/2021

---
+++ 09042021.R
@@ -0,0 +1,21 @@
+2+2
+10*8
+10/2
+11/2
+10%%2
+10%%4
+substratct <- 10-20
+abs(substratct)
+xyz = 4
+.abc = 2
+p.a=1
+abc <- .abc + 2
+abc ^ 10
+abc + 10
+abc *2
+abc <- abc+10
+abc * 2
+abc <<- 2 *2
+abc
+test <- "parimal"
+test <- test & 10
